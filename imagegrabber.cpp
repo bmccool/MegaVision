38ae27df1cb6625c8e1d6a3596b5b1e09641633d
EUCLID\Brendon <brendonmccool@gmail.com>
2014-08-26 21:29:36 -0400

    Pruned unwanted code from the starting image.  Just want to get image capture working for the moment.

--- imagegrabber.cpp
+++ imagegrabber.cpp
@@ -13,7 +13,6 @@ using namespace cv;
 int low [3] =  {0, 21, 66};
 int high[3] = {61, 50, 95};
 
-// TODO: Make sure these are the correct resolution for our camera
 double width = 176;
 double height = 144;
 
@@ -36,41 +35,11 @@ int main()
 	// Grab the Image
 	cout << "Grab the Image" << endl;
 	Mat imgInput, imgHSV;
-	capture >>imgInput;
+	capture >> imgInput;
 
-	// Convert from BGR to HSV ColorSpace
-	cout << "Convert from BGR to HSV ColorSpace" << endl;
-	cvtColor(imgInput, imgHSV, CV_BGR2HSV);
-
-	// Split into color arrays
-	cout << "Split into color arrays" <<endl;
-	Mat planes[3];
-	split(imgHSV, planes);
-
-	// Threshold the image
-	cout << "Threshold the image" << endl;
-	for (int i = 0; i < 3; i++)
-	{
-		Mat imgThreshLow, imgThreshHigh;
-		threshold(planes[i], imgThreshLow, low[i], 255, THRESH_BINARY);
-		threshold(planes[i], imgThreshHigh, high[i], 255, THRESH_BINARY);
-		bitwise_and(imgThreshLow, imgThreshHigh, planes[i]);
-	}
-
-	// Create the final result
-	cout << "Create the final result" << endl;
-	Mat imgResult(Size(width, height), CV_8UC1, Scalar(255));
-	for (int i = 0; i < 3; i++)
-	{
-		cout << "Looping through image creation..." << endl;
-		bitwise_and(imgResult, planes[i], imgResult);
-	}
-
-	// Write the images
-	cout << "Writing Images" << endl;
+	// Write the image
+	cout << "Writing Image" << endl;
 	imwrite("imgInput.png", imgInput);
-	imwrite("imgHSV.png", imgHSV);
-	imwrite("imgResult.png", imgResult);
 
 	// DONE
 	return 0;
