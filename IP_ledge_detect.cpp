05b66d0175d26246f9b5389106c2ab4fe301fccb
EUCLID\Brendon <bmccool@users.noreply.github.com>
2014-09-10 19:35:20 -0400

    Revising vertical line conditional.

--- IP_ledge_detect.cpp
+++ IP_ledge_detect.cpp
@@ -58,8 +58,9 @@ int detect_lines(Mat src)
     for( size_t i = 0; i < lines.size(); i++ )
     {
         Vec4i l = lines[i];
-        if (!(l[0] - l[2] == 0) && // If the line is NOT purely vertical and...
-           (((l[1] - l[3]) / (l[0] - l[2])) < .5)) // If the slope of the line is less than 1/2.
+        if (!(l[0] - l[2] == 0) && // If the line is NOT purely vertical (div by zero protection) and...
+           (((l[1] - l[3]) / (l[0] - l[2])) < 0.5) && // If the slope of the line is less than 1/2 and...
+           (((l[1] - l[3]) / (l[0] - l[2])) > -0.5)) // If the slope of the line is greater than 1/2.
         {
             // only draw lines with slope less than 1/2
             line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,255,0), 1, CV_AA);
