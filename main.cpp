30bbfad8981a6364cb87c7f45dcdebedb9cce0c5
EUCLID\Brendon <brendonmccool@gmail.com>
2014-09-17 18:19:17 -0400

    Cleaning up main.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,10 @@ using namespace cv;
 
 #define SLIDER_MAX 100
 
+// Local Function Prototypes
+int main_polling(VideoCapture *capture);
+int main_looping(VideoCapture *capture, char *filename);
+
 int main(int argc, char* argv[])
 {
     VideoCapture cap;  // Capture Object
@@ -24,9 +28,9 @@ int main(int argc, char* argv[])
     if (argc < 2)
     {
         // No arguments were passed
-    // Grab an image and exit
+        // Grab an image and exit
         
-    // Get a new frame from the camera
+        // Get a new frame from the camera
         cout << "Get a new frame from the camera" << endl;
         Mat frame;
         cap >> frame;
@@ -39,68 +43,11 @@ int main(int argc, char* argv[])
     {
         if (string(argv[1]) == "p")
         {
-            // Polling mode
-            // This mode will poll the user for a number
-            // then take that many number of pictures.
-            // This mode is intended to get samples of
-            // frames that we can use when designing the
-            // processing component of the project.
-            int num_frames = 0;
-            char OutString[30];
-            int i;
-            Mat frame;
-            do
-            {
-                cout << "Select number of frames: ";
-                cin  >> num_frames;
-                
-                for (i = 0; i < num_frames; i++)
-                {
-                    cap >> frame;
-                    sprintf(OutString, "FrameGrab_%d.png", i);
-                    imwrite(OutString, frame);
-                }
-            }while(num_frames > 0);
+            main_polling(&cap);
         }
         else if (string(argv[1]) == "l")
         {
-            
-            // Read the input image from file
-            cout << "Read the input image from file" << endl;
-            Mat ReadFrame = imread(string(argv[2]), 0);
-            // Verify imread is successful
-            if (!ReadFrame.data)
-            {
-                cout <<  "Could not open or find the image" << endl;
-                return -1;
-            }
-            
-            // Var mat to hold the output image
-            Mat output_mat;
-            // Buffer for output filename
-            char OutString[50];
-
-            // Global Variables
-            int max_line_length = 10;
-            int max_line_gap = 5;
-            int threshold_var = 5;
-
-            
-            for (max_line_length = 10; max_line_length < 100; max_line_length += 10)
-            {
-                for (max_line_gap = 5; max_line_gap < 100; max_line_gap += 10)
-                {
-                    for (threshold_var = 5; threshold_var < 100; threshold_var += 10)
-                    {
-                        // Detect the lines in this frame
-                        detect_lines_hough_prob(ReadFrame, max_line_length, max_line_gap, threshold_var, output_mat);
-                        // Create the output filename
-                        sprintf(OutString, "length %d gap %d thresh %d.png", max_line_length, max_line_gap, threshold_var);
-                        // Write the image
-                        imwrite(OutString, output_mat);
-                    }
-                }
-            }
+            main_looping(&cap, string(argv[2]));
         }
     }
 
@@ -112,3 +59,67 @@ int main(int argc, char* argv[])
 
 }
 
+int main_polling(VideoCapture *capture)
+{
+    // Polling mode
+    // This mode will poll the user for a number
+    // then take that many number of pictures.
+    // This mode is intended to get samples of
+    // frames that we can use when designing the
+    // processing component of the project.
+    int num_frames = 0;
+    char OutString[30];
+    int i;
+    Mat frame;
+    do
+    {
+        cout << "Select number of frames: ";
+        cin  >> num_frames;
+                
+        for (i = 0; i < num_frames; i++)
+        {
+            cap >> frame;
+            sprintf(OutString, "FrameGrab_%d.png", i);
+            imwrite(OutString, frame);
+        }
+    }while(num_frames > 0);
+}
+
+int main_looping(VideoCapture *capture, char *filename)
+{
+    // Read the input image from file
+    cout << "Read the input image from file" << endl;
+    Mat ReadFrame = imread(filename, 0);
+    // Verify imread is successful
+    if (!ReadFrame.data)
+    {
+        cout <<  "Could not open or find the image" << endl;
+        return -1;
+    }
+            
+    // Var mat to hold the output image
+    Mat output_mat;
+    // Buffer for output filename
+    char OutString[50];
+
+    // Line Transform Parameters
+    int max_line_length = 10;
+    int max_line_gap = 5;
+    int threshold_var = 5;
+
+    for (max_line_length = 10; max_line_length < 100; max_line_length += 10)
+    {
+        for (max_line_gap = 5; max_line_gap < 100; max_line_gap += 10)
+        {
+            for (threshold_var = 5; threshold_var < 100; threshold_var += 10)
+            {
+                // Detect the lines in this frame
+                detect_lines_hough_prob(ReadFrame, max_line_length, max_line_gap, threshold_var, output_mat);
+                // Create the output filename
+                sprintf(OutString, "length %d gap %d thresh %d.png", max_line_length, max_line_gap, threshold_var);
+                // Write the image
+                imwrite(OutString, output_mat);
+            }
+        }
+    }
+}
