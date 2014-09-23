4f1e216e853b65ef97b3547dfcf9fa56fb9a9d48
Brendon McCool <bmccool@users.noreply.github.com>
2014-09-23 14:37:16 +0000

    Modifying headers for previous change to find_contours().

--- IP_ledge_detect.hpp
+++ IP_ledge_detect.hpp
@@ -25,5 +25,6 @@ int detect_lines_hough_prob(cv::Mat src, int max_line_length, int max_line_gap, 
 int detect_lines_hough(cv::Mat src, int max_line_length, int max_line_gap, int threshold_var, cv::Mat& output_mat);
 contours_t find_countours(cv::Mat input_mat, cv::Mat & output_mat);
 contours_t remove_duplicate_contours(contours_t contours_old, contours_t contours_new);
+void draw_contours(cv::Mat input_mat, cv::Mat & output_mat, contours_t contours);
 
 #endif
