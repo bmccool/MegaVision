9894194cd583cd6447844bcb43f508f1e3e9fcd7
EUCLID\Brendon <brendonmccool@gmail.com>
2014-09-24 21:15:18 -0400

    Getting rid of extraneous outputs.

--- main.cpp
+++ main.cpp
@@ -264,7 +264,10 @@ void main_wobble(VideoCapture & capture)
         frame.copyTo(frame_old);
         capture >> frame;
         pixels = sidescroll_right_gray(frame_old, frame);
-        cout << "The image has advanced " << pixels << " pixels." << endl;
+        if (pixels != 0)
+        {
+            cout << "The image has advanced " << pixels << " pixels." << endl;
+        }
         bg.operator ()(frame,fore);
         bg.getBackgroundImage(back);
         erode(fore,fore,Mat());
