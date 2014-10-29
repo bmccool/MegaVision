255fc3fe5af952ae6b15e84320159ca6a09cea01
Brendon <bmccool@users.noreply.github.com>
2014-10-29 18:48:12 -0400

    Fixed segfault. Added -g flag to Makefile.

---
+++ CT_window_manager.hpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+* CT_window_manager.hpp
+* This header contains prototypes and structures needed by the file
+* CT_window_manager.cpp.  This set of functions is used by the program in a
+* Ubuntu Desktop environment to find and manage windows.
+*
+******************************************************************************/
+// Make sure this is only included once
+#ifndef CT_WINDOW_MANAGER_HPP
+#define CT_WINDOW_MANAGER_HPP
+
+
+/////////////////////////// STANDARD INCLUDES  ////////////////////////////////
+/////////////////////////// PROJECT INCLUDES   ////////////////////////////////
+// Exported function prototypes
+int send_key_down(char* keycode, unsigned long int window_id);
+int send_key_up(char* keycode, unsigned long int window_id);
+int send_key(char* keycode, unsigned long int window_id);
+unsigned long int get_nes_window(void);
+
+#endif
